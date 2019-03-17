9966c29a4cd2e260bc7dadd85cfa71f4f5246cb4
Scovia <achanscovia@gmail.com>
2019-03-17 03:10:38 +0300

    Tests completed

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,22 @@
+
+def fizzbuzz(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return "Invalid input"
+
+    elif isinstance(a, list) and isinstance(b, int):
+        return "Invalid input"
+
+    elif isinstance(a, list) and isinstance(b, list):
+        
+        y = len(a) + len(b)
+
+        if y%3 == 0:
+            if y%5 == 0:
+                return "fizzbuzz"
+            return "fizz"
+        elif y%5 == 0:
+            return "buzz"
+
+        elif y%3 != 0: 
+            return y
+
---
+++ vowels.py
@@ -0,0 +1,21 @@
+def countVowels(word):
+    if isinstance(word,str):
+
+    
+        vowels = "aeiou"
+        # changing the word into lower case
+        new_word = word.lower()
+        # the set property ensures there is no duplicate letter in the string
+        no_duplicates = set(new_word)
+        duplicates = len(new_word) - len(no_duplicates)
+        vowel_string = ""
+
+        for letter in vowels:
+            if letter in no_duplicates:
+                vowel_string += str(letter)
+
+        return (vowel_string, duplicates)
+    else:
+        return "invalid input"
+
+print(countVowels('didida'))
